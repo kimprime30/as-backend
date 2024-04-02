b4c1b835022c3f664a133a613283522dfb9dc1d8
Kimprime30 <kin666@gmail.com>
2024-04-02 08:23:51 -0300

    initial commit

---
+++ src/controllers/auth.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from "express";
+import { z } from "zod";
+import * as auth from '../services/auth'
+
+export const login: RequestHandler = (req, res) => {
+  const loginSchema = z.object({
+    password: z.string()
+  });
+
+  const body = loginSchema.safeParse(req.body);
+  if (!body.success) return res.json({ error: 'Dados inválidos' })
+
+  if (!auth.validatePassword(body.data.password)) {
+    return res.status(403).json({ error: 'acesso negado' })
+  }
+  res.json({ token: auth.createToken() }) //const token = auth.createToken(); Envie o token na resposta  res.json({ token })
+}
+
+export const validate: RequestHandler = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ error: 'Acesso negado' })
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+  if (!auth.validateToken(token)) {
+    return res.status(403).json({ error: 'Acesso negado' })
+  }
+  next();
+}
